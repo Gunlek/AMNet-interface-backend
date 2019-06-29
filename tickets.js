30438d5c9322ded1aa9ecf51213cee305c6c4e12
Fabien Aubret <fabien.aubret@gmail.com>
2019-06-29 19:04:20 +0200

    [Add] Add administration capabilities for tickets

--- tickets.js
+++ tickets.js
@@ -114,8 +114,8 @@ module.exports = (app) => {
             }
             else {
                 req.session.returnTo = '/tickets/admin-tickets/';
-                connection.query('SELECT * FROM tickets WHERE state=1', (errors, closed_tickets, fields) => {
-                    connection.query('SELECT * FROM tickets WHERE state=0', (errors, opened_tickets, fields) => {
+                connection.query('SELECT * FROM tickets WHERE ticket_state=1', (errors, closed_tickets, fields) => {
+                    connection.query('SELECT * FROM tickets WHERE ticket_state=0', (errors, opened_tickets, fields) => {
                         res.render('tickets/admin-tickets.html.twig', {data: req.session, opened_tickets: opened_tickets, closed_tickets: closed_tickets});
                     });
                 });
