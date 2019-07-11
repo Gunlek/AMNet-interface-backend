115c7be6b6e188178f4254acf1ea829d9148a444
Fabien Aubret <fabien.aubret@gmail.com>
2019-07-11 19:06:00 +0200

    [Add] Administration capabilities for access

--- access.js
+++ access.js
@@ -3,12 +3,13 @@ let md5 = require('md5');
 let bodyParser = require('body-parser');
 let urlencodedParser = bodyParser.urlencoded({ extended: false});
 let session = require('express-session');
+require('dotenv').config();
 
 let connection = mysql.createConnection({
-    host    :   'localhost',
-    user    :   'root',
-    password:   '',
-    database:   'amnet_birse'
+    host    :   process.env.DB_HOST,
+    user    :   process.env.DB_USER,
+    password:   process.env.DB_PASS,
+    database:   process.env.DB_NAME
 });
 
 connection.connect();
@@ -79,6 +80,73 @@ module.exports = (app) => {
         }
         else
             res.redirect('/access/signin/?state=failed');
+    });
+
+    app.get('/internet/admin-access/', (req, res) => {
+        if(!req.session['logged_in']){
+            req.session.returnTo = '/internet/admin-access/';
+            res.redirect('/access/login/');
+        }
+        else {
+            if(req.session['user_rank'] != "admin")
+            {
+                res.redirect('/');
+            }
+            else {
+                req.session.returnTo = '/access/admin-access/';
+                connection.query('SELECT * FROM access', (errors, requests, fields) => {
+                    connection.query('SELECT * FROM users', (errors, users, fields) => {
+                        let user_list = {};
+                        for(let k = 0; k < users.length; k++){
+                            let user = users[k];
+                            user_list[user['user_id']] = {user_id: user['user_id'], user_name: user['user_name'], user_bucque: user['user_bucque'], user_fams: user['user_fams'], user_proms: user['user_proms'], user_rank: user['user_rank']}
+                            if(k == users.length - 1)
+                                res.render('internet/admin-access.html.twig', {data: req.session, requests: requests, user_list: user_list});
+                        }
+                    });
+                });
+            }
+        }
+    });
+
+    app.get('/internet/allow/:access_id', (req, res) => {
+        if(!req.session['logged_in']){
+            req.session.returnTo = '/internet/admin-access/';
+            res.redirect('/access/login/');
+        }
+        else {
+            if(req.session['user_rank'] != "admin")
+            {
+                res.redirect('/');
+            }
+            else {
+                let access_id = parseInt(req.params.access_id);
+                console.log(access_id);
+                connection.query('UPDATE access SET access_state = "active" WHERE access_id = ?', [access_id], () => {
+                    res.redirect('/internet/admin-access');
+                });
+            }
+        }
+    });
+
+    app.get('/internet/disallow/:access_id', (req, res) => {
+        if(!req.session['logged_in']){
+            req.session.returnTo = '/internet/admin-access/';
+            res.redirect('/access/login/');
+        }
+        else {
+            if(req.session['user_rank'] != "admin")
+            {
+                res.redirect('/');
+            }
+            else {
+                let access_id = parseInt(req.params.access_id);
+                console.log(access_id);
+                connection.query('UPDATE access SET access_state = "suspended" WHERE access_id = ?', [access_id], () => {
+                    res.redirect('/internet/admin-access');
+                });
+            }
+        }
     });
 
 }
