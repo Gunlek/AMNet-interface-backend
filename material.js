726f0a03e04a083fbf35e0e2ea18ba97373734d6
Fabien Aubret <fabien.aubret@gmail.com>
2019-09-06 09:39:51 +0200

    [Add] Add admin request handling

--- material.js
+++ material.js
@@ -50,4 +50,69 @@ module.exports = (app) => {
             res.redirect('/material/list-requests/');
         });
     });
+
+    app.get('/material/admin-requests/', (req, res) => {
+        if(!req.session['logged_in']){
+            req.session.returnTo = '/material/admin-requests/';
+            res.redirect('/access/login/');
+        }
+        else {
+            if(req.session['user_rank'] != "admin")
+            {
+                res.redirect('/');
+            }
+            else {
+                req.session.returnTo = '/access/admin-access/';
+                connection.query('SELECT * FROM materials', (errors, requests, fields) => {
+                    connection.query('SELECT * FROM users', (errors, users, fields) => {
+                        let user_list = {};
+                        for(let k = 0; k < users.length; k++){
+                            let user = users[k];
+                            user_list[user['user_id']] = {user_id: user['user_id'], user_name: user['user_name'], user_bucque: user['user_bucque'], user_fams: user['user_fams'], user_proms: user['user_proms'], user_rank: user['user_rank']}
+                            if(k == users.length - 1)
+                                res.render('material/admin-requests.html.twig', {data: req.session, requests: requests, user_list: user_list});
+                        }
+                    });
+                });
+            }
+        }
+    });
+
+    app.get('/material/allow/:material_id', (req, res) => {
+        if(!req.session['logged_in']){
+            req.session.returnTo = '/material/admin-requests/';
+            res.redirect('/access/login/');
+        }
+        else {
+            if(req.session['user_rank'] != "admin")
+            {
+                res.redirect('/');
+            }
+            else {
+                let material_id = parseInt(req.params.material_id);
+                connection.query('UPDATE materials SET material_state = "granted" WHERE material_id = ?', [material_id], () => {
+                    res.redirect('/material/admin-requests/');
+                });
+            }
+        }
+    });
+
+    app.get('/material/disallow/:material_id', (req, res) => {
+        if(!req.session['logged_in']){
+            req.session.returnTo = '/material/admin-requests/';
+            res.redirect('/access/login/');
+        }
+        else {
+            if(req.session['user_rank'] != "admin")
+            {
+                res.redirect('/');
+            }
+            else {
+                let material_id = parseInt(req.params.material_id);
+                connection.query('UPDATE materials SET material_state = "declined" WHERE material_id = ?', [material_id], () => {
+                    res.redirect('/material/admin-requests/');
+                });
+            }
+        }
+    });
 }
