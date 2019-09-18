64ae4dc50bf13a2f2806a08d5dc97794c80554f0
Fabien Aubret <fabien.aubret@gmail.com>
2019-09-18 09:54:44 +0200

    [Fix] Fix bad indexation on tables

--- material.js
+++ material.js
@@ -63,15 +63,19 @@ module.exports = (app) => {
             }
             else {
                 req.session.returnTo = '/access/admin-access/';
-                connection.query('SELECT * FROM materials', (errors, requests, fields) => {
-                    connection.query('SELECT * FROM users', (errors, users, fields) => {
-                        let user_list = {};
-                        for(let k = 0; k < users.length; k++){
-                            let user = users[k];
-                            user_list[user['user_id']] = {user_id: user['user_id'], user_name: user['user_name'], user_bucque: user['user_bucque'], user_fams: user['user_fams'], user_proms: user['user_proms'], user_rank: user['user_rank']}
-                            if(k == users.length - 1)
-                                res.render('material/admin-requests.html.twig', {data: req.session, requests: requests, user_list: user_list});
-                        }
+                connection.query('SELECT * FROM materials WHERE material_state="granted"', (errors, granted_requests, fields) => {
+                    connection.query('SELECT * FROM materials WHERE material_state="declined"', (errors, declined_requests, fields) => {
+                        connection.query('SELECT * FROM materials WHERE material_state="pending"', (errors, pending_requests, fields) => {
+                            connection.query('SELECT * FROM users', (errors, users, fields) => {
+                                let user_list = {};
+                                for(let k = 0; k < users.length; k++){
+                                    let user = users[k];
+                                    user_list[user['user_id']] = {user_id: user['user_id'], user_name: user['user_name'], user_bucque: user['user_bucque'], user_fams: user['user_fams'], user_proms: user['user_proms'], user_rank: user['user_rank']}
+                                    if(k == users.length - 1)
+                                        res.render('material/admin-requests.html.twig', {data: req.session, granted_requests: granted_requests, declined_requests: declined_requests, pending_requests: pending_requests, user_list: user_list});
+                                }
+                            });
+                        });
                     });
                 });
             }
