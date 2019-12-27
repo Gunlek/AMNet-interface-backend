2262af041d8718020a88e62bf5b69f9cf4a352c3
Fabien Aubret <fabien.aubret@gmail.com>
2019-12-27 15:29:35 +0100

    [Update] Change users to access for consistency

--- users.js
+++ users.js
@@ -37,8 +37,8 @@ module.exports = (app) => {
     /*
      * Displays lost-password page
      */
-    app.get('/access/lost_password/', (req, res) => {
-        res.render('access/lost-password.html.twig', {data: req.session});
+    app.get('/users/lost_password/', (req, res) => {
+        res.render('users/lost-password.html.twig', {data: req.session});
     });
 
     /*
@@ -47,7 +47,7 @@ module.exports = (app) => {
      * to the resetting request and send an email to this address using
      * the specified GMail account containing the address to the specific token
      */
-    app.post('/access/process_lost_password/', urlencodedParser, (req, res) => {
+    app.post('/users/process_lost_password/', urlencodedParser, (req, res) => {
         let email = req.body.email;
         let token_value = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
         connection.query('SELECT user_id FROM users WHERE user_email = ?', [email], (error, results, fields) => {
@@ -73,19 +73,19 @@ module.exports = (app) => {
                 htmlstream.close();
             }
         });
-        res.redirect('/access/login/');
+        res.redirect('/users/login/');
     });
 
     /*
      * Displays the reset-password page that correspond to the specified
      * token (via GET)
      */
-    app.get('/access/change_password/:token', (req, res ) => {
+    app.get('/users/change_password/:token', (req, res ) => {
         let token = req.params.token;
         let password_change_failed = false;
         if(req.query.state != null && req.query.state === "failed")
             password_change_failed = true;
-        res.render('access/change_password.html.twig', {data: req.session, token: token, password_change_failed: password_change_failed});
+        res.render('users/change_password.html.twig', {data: req.session, token: token, password_change_failed: password_change_failed});
     });
 
     /*
@@ -93,7 +93,7 @@ module.exports = (app) => {
      * Reset the password in SQL, gathering corresponding user (linked via token) and
      * update its account password
      */
-    app.post('/access/update_password', urlencodedParser, (req, res) => {
+    app.post('/users/update_password', urlencodedParser, (req, res) => {
         let password = md5(req.body.password);
         let conf_password = md5(req.body.conf_password);
         let token = req.body.token;
@@ -102,44 +102,44 @@ module.exports = (app) => {
                 if(results.length > 0){
                     connection.query("UPDATE users SET user_password = ? WHERE user_id = ?", [password, results[0]["token_user"]], () => {
                         connection.query("DELETE FROM reset_token WHERE token_value = ?", [token], () => {
-                            res.redirect('/access/login');
+                            res.redirect('/users/login');
                         });
                     });
                 }
             });
         }
         else{
-            res.redirect('/access/change_password/'+token+'?state=failed');
+            res.redirect('/users/change_password/'+token+'?state=failed');
         }
     });
 
     /*
      * Displays the log-in page
      */
-    app.get('/access/login/', (req, res) => {
+    app.get('/users/login/', (req, res) => {
         let login_failed = false;
         if(req.query.state != null && req.query.state === "failed")
             login_failed = true;
-        res.render('access/login.html.twig', {data: req.session, login_failed: login_failed});
+        res.render('users/login.html.twig', {data: req.session, login_failed: login_failed});
     });
     
     /*
      * Displays the sign-in page
      */
-    app.get('/access/signin/', (req, res) => {
+    app.get('/users/signin/', (req, res) => {
         let signin_failed = false;
         let no_charte = false;
         if(req.query.state != null && req.query.state === "signin_failed")
             signin_failed = true;
         if(req.query.state != null && req.query.state === "no_charte")
             no_charte = true;
-        res.render('access/signin.html.twig', {data: req.session, signin_failed: signin_failed, no_charte: no_charte});
+        res.render('users/signin.html.twig', {data: req.session, signin_failed: signin_failed, no_charte: no_charte});
     });
 
     /*
      * Handle disconnection requests and redirect user to index
      */
-    app.get('/access/disconnect/', (req, res) => {
+    app.get('/users/disconnect/', (req, res) => {
         req.session['logged_in'] = false;
         req.session['user_id'] = -1;
         req.session['user_name'] = "";
@@ -150,7 +150,7 @@ module.exports = (app) => {
      * Handle POST request from log-in page
      * Check if they are registered in the database and if username/password corresponds
      */
-    app.post('/access/process_login/', urlencodedParser, (req, res) => {
+    app.post('/users/process_login/', urlencodedParser, (req, res) => {
         connection.query('SELECT * FROM users WHERE user_name=? AND user_password=?', [req.body.username, md5(req.body.password)], (error, results, fields) => {
             if(results.length > 0)
             {
@@ -164,7 +164,7 @@ module.exports = (app) => {
                 res.redirect(returnURL);
             }
             else
-                res.redirect('/access/login/?state=failed');
+                res.redirect('/users/login/?state=failed');
         });
     });
 
@@ -174,7 +174,7 @@ module.exports = (app) => {
      * form and check if password and conf_password corresponds
      * then register the newly created user to the database and redirect to log-in
      */
-    app.post('/access/process_signin/', urlencodedParser, (req, res) => {
+    app.post('/users/process_signin/', urlencodedParser, (req, res) => {
         let username = req.body.username;
         let firstname = req.body.firstname;
         let lastname = req.body.lastname;
@@ -191,27 +191,27 @@ module.exports = (app) => {
                 connection.query('SELECT * FROM users WHERE user_name=?', [username], function(errors, results, fields){
                     if(results.length == 0){
                         connection.query('INSERT INTO users(user_name, user_firstname, user_lastname, user_email, user_password, user_bucque, user_fams, user_proms) VALUES(?, ?, ?, ?, ?, ?, ?, ?)', [username, firstname, lastname, email, password, bucque, fams, proms]);
-                        res.redirect('/access/login/');
+                        res.redirect('/users/login/');
                     }
                     else
-                        res.redirect('/access/signin/?state=signin_failed');
+                        res.redirect('/users/signin/?state=signin_failed');
                 });
             }
             else
-                res.redirect('/access/signin/?state=no_charte');
+                res.redirect('/users/signin/?state=no_charte');
         }
         else
-            res.redirect('/access/signin/?state=signin_failed');
+            res.redirect('/users/signin/?state=signin_failed');
     });
 
     /*
      * Displays the specific profile page
      * Allows admin to check a specific profile from tables
      */
-    app.get('/user/profile/:user_id', (req, res) => {
+    app.get('/users/profile/:user_id', (req, res) => {
         if(!req.session['logged_in']){
-            req.session.returnTo = '/user/profile/'+req.params.user_id;
-            res.redirect('/access/login/');
+            req.session.returnTo = '/users/profile/'+req.params.user_id;
+            res.redirect('/users/login/');
         }
         else {
             if(req.session['user_rank'] != "admin")
@@ -222,7 +222,7 @@ module.exports = (app) => {
                 if(req.params.user_id != ""){
                     connection.query('SELECT * FROM users WHERE user_id = ?', [parseInt(req.params.user_id)], (errors, results, fields) => {
                         if(results.length > 0)
-                            res.render('access/profile.html.twig', {data: req.session, user_data: results[0]})
+                            res.render('users/profile.html.twig', {data: req.session, user_data: results[0]})
                     });
                 }
             }
@@ -234,10 +234,10 @@ module.exports = (app) => {
      * Displays all their attributes and the number of registered
      * MAC address per account
      */
-    app.get('/access/list-users/', (req, res) => {
+    app.get('/users/list-users/', (req, res) => {
         if(!req.session['logged_in']){
-            req.session.returnTo = '/access/list-users/';
-            res.redirect('/access/login/');
+            req.session.returnTo = '/users/list-users/';
+            res.redirect('/users/login/');
         }
         else {
             if(req.session['user_rank'] != "admin")
@@ -246,7 +246,7 @@ module.exports = (app) => {
             }
             else {
                 connection.query('SELECT *, COUNT(access_id) AS mac_count FROM `users` LEFT JOIN `access` ON users.user_id=access.access_user GROUP BY user_id', (errors, results, fields) => {
-                    res.render('access/list_users.html.twig', {data: req.session, user_list: results});
+                    res.render('users/list_users.html.twig', {data: req.session, user_list: results});
                 });
             }
         }
@@ -255,10 +255,10 @@ module.exports = (app) => {
     /*
      * Delete a user from users table based on its user_id (passed over GET)
      */
-    app.get('/access/delete-user/:user_id', (req, res) => {
+    app.get('/users/delete-user/:user_id', (req, res) => {
         if(!req.session['logged_in']){
-            req.session.returnTo = '/access/list-users/';
-            res.redirect('/access/login/');
+            req.session.returnTo = '/users/list-users/';
+            res.redirect('/users/login/');
         }
         else {
             if(req.session['user_rank'] != "admin")
@@ -267,7 +267,7 @@ module.exports = (app) => {
             }
             else {
                 connection.query('DELETE FROM users WHERE user_id = ?', [req.params.user_id], () => {
-                    res.redirect('/access/list-users/');
+                    res.redirect('/users/list-users/');
                 });
             }
         }
@@ -278,12 +278,12 @@ module.exports = (app) => {
      * To make them admin or downgrade them back to user
      * data (user_id and new user_rank) are passed over GET request
      */
-    app.get('/access/grant-user/:user_id/:user_rank', (req, res) => {
+    app.get('/users/grant-user/:user_id/:user_rank', (req, res) => {
         let user_rank = req.params.user_rank;
         let user_id = req.params.user_id;
         if(!req.session['logged_in']){
-            req.session.returnTo = '/access/list-users/';
-            res.redirect('/access/login/');
+            req.session.returnTo = '/users/list-users/';
+            res.redirect('/users/login/');
         }
         else {
             if(req.session['user_rank'] != "admin")
@@ -292,7 +292,7 @@ module.exports = (app) => {
             }
             else {
                 connection.query('UPDATE users SET user_rank = ? WHERE user_id = ?', [user_rank, user_id], () => {
-                    res.redirect('/access/list-users/');
+                    res.redirect('/users/list-users/');
                 });
             }
         }
