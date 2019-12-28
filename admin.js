180a7f43f10e2c5ab613d76b34b028b0fa921489
Fabien Aubret <fabien.aubret@gmail.com>
2019-12-28 15:49:23 +0100

    [Add] Add statistics to admin index

--- admin.js
+++ admin.js
@@ -18,7 +18,9 @@ connection.connect();
 module.exports = (app) => {
 
     app.use(session({
-        secret: "amnet-interface"
+        secret: "amnet-interface",
+        resave: false,
+        saveUninitialized: false,
     }));
 
     /*
@@ -33,7 +35,22 @@ module.exports = (app) => {
             res.redirect('/');
         }
         else {
-            res.render('admin/admin-index.html.twig', {data: req.session});
+            connection.query('SELECT COUNT(*) as nb_user FROM users', (error, nb_users_result, fields) => {
+                let nb_users = nb_users_result[0]['nb_user'];
+                connection.query('SELECT COUNT(*) as nb_cotiz FROM users WHERE user_pay_status=0', (error, nb_cotiz_results, fields) => {
+                    let nb_cotiz = nb_cotiz_results[0]['nb_cotiz'];
+                    connection.query('SELECT COUNT(*) as nb_access_request FROM access WHERE access_state="pending"', (error, nb_access_request_results, fields) => {
+                        let nb_access_request = nb_access_request_results[0]['nb_access_request'];
+                        connection.query('SELECT COUNT(*) as nb_material_request FROM materials WHERE material_state="pending"', (error, nb_material_request_result, fields) => {
+                            let nb_material_request = nb_material_request_result[0]['nb_material_request'];
+                            connection.query('SELECT COUNT(*) as nb_tickets FROM tickets WHERE ticket_state=1', (error, nb_tickets_results, fields) => {
+                                let nb_tickets = nb_tickets_results[0]['nb_tickets'];
+                                res.render('admin/admin-index.html.twig', {data: req.session, nb_users: nb_users, nb_cotiz: nb_cotiz, nb_access_request: nb_access_request, nb_material_request: nb_material_request, nb_tickets: nb_tickets});
+                            });
+                        });
+                    });
+                });
+            });
         }
     });
 }
