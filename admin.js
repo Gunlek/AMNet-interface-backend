7dcc016c7cf05cc7a1533a71bb86c970825842cc
Fabien Aubret <fabien.aubret@gmail.com>
2019-12-29 13:30:45 +0100

    [Add] Add material management capabilities

--- admin.js
+++ admin.js
@@ -160,4 +160,106 @@ module.exports = (app) => {
             }
         }
     });
+
+    /*
+     * Gather all the currently saved requests of hardware and display them
+     * in tables to allow easier management from administrators
+     */
+    app.get('/admin/material/', (req, res) => {
+        if(!req.session['logged_in']){
+            req.session.returnTo = '/admin/material/';
+            res.redirect('/users/login/');
+        }
+        else {
+            if(req.session['user_rank'] != "admin")
+            {
+                res.redirect('/');
+            }
+            else {
+                req.session.returnTo = '/admin/material/';
+                connection.query('SELECT * FROM materials WHERE material_state="granted"', (errors, granted_requests, fields) => {
+                    connection.query('SELECT * FROM materials WHERE material_state="declined"', (errors, declined_requests, fields) => {
+                        connection.query('SELECT * FROM materials WHERE material_state="pending"', (errors, pending_requests, fields) => {
+                            connection.query('SELECT * FROM users', (errors, users, fields) => {
+                                let user_list = {};
+                                for(let k = 0; k < users.length; k++){
+                                    let user = users[k];
+                                    user_list[user['user_id']] = {user_id: user['user_id'], user_name: user['user_name'], user_bucque: user['user_bucque'], user_fams: user['user_fams'], user_proms: user['user_proms'], user_rank: user['user_rank']}
+                                    if(k == users.length - 1)
+                                        res.render('admin/admin-material.html.twig', {data: req.session, granted_requests: granted_requests, declined_requests: declined_requests, pending_requests: pending_requests, user_list: user_list});
+                                }
+                            });
+                        });
+                    });
+                });
+            }
+        }
+    });
+
+    /*
+     * Allows an administrator to mark a hardware request as "agreed"
+     */
+    app.get('/admin/material/allow/:material_id', (req, res) => {
+        if(!req.session['logged_in']){
+            req.session.returnTo = '/admin/material/';
+            res.redirect('/users/login/');
+        }
+        else {
+            if(req.session['user_rank'] != "admin")
+            {
+                res.redirect('/');
+            }
+            else {
+                let material_id = parseInt(req.params.material_id);
+                connection.query('UPDATE materials SET material_state = "granted" WHERE material_id = ?', [material_id], () => {
+                    res.redirect('/admin/material/');
+                });
+            }
+        }
+    });
+
+    /*
+     * Allows an administrator to mark a hardware request as "declined"
+     */
+    app.get('/admin/material/disallow/:material_id', (req, res) => {
+        if(!req.session['logged_in']){
+            req.session.returnTo = '/admin/material/';
+            res.redirect('/users/login/');
+        }
+        else {
+            if(req.session['user_rank'] != "admin")
+            {
+                res.redirect('/');
+            }
+            else {
+                let material_id = parseInt(req.params.material_id);
+                connection.query('UPDATE materials SET material_state = "declined" WHERE material_id = ?', [material_id], () => {
+                    res.redirect('/admin/material/');
+                });
+            }
+        }
+    });
+
+    /*
+     * Delete a hardware request created by user
+     * Only possible for administrators
+     */
+    app.get('/admin/material/delete/:material_id', (req, res) => {
+        if(!req.session['logged_in']){
+            req.session.returnTo = '/admin/material/';
+            res.redirect('/users/login/');
+        }
+        else {
+            if(req.session['user_rank'] != "admin")
+            {
+                res.redirect('/');
+            }
+            else {
+                let material_id = parseInt(req.params.material_id);
+                connection.query('DELETE FROM materials WHERE material_id = ?', [material_id], () => {
+                    res.redirect('/admin/material/');
+                });
+            }
+        }
+    });
 }
