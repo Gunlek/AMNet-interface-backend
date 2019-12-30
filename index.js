62012f3880b81309c2e997575dedd0aafc6bcf16
Fabien Aubret <fabien.aubret@gmail.com>
2019-12-30 11:37:50 +0100

    [Add] Add news message

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+let mysql = require('mysql');
 var express = require('express');
 let session = require('express-session');
+require('dotenv').config();
+
+let connection = mysql.createConnection({
+    host    :   process.env.DB_HOST,
+    user    :   process.env.DB_USER,
+    password:   process.env.DB_PASS,
+    database:   process.env.DB_NAME
+});
+
+connection.connect();
 
 var app = express();
 
@@ -20,8 +31,16 @@ app.use(express.static('statics'));
 app.get('/', (req, res) => {
     if(!req.session['logged_in'])
         res.redirect('users/login/');
-    else
-        res.render('index.html.twig', {data: req.session});
+    else {
+        connection.query('SELECT * FROM settings', (error, settings_results, fields) => {
+            let settings = {};
+            settings_results.forEach(param => {
+                settings[param['setting_name']] = param['setting_value']
+            });
+            
+            res.render('index.html.twig', {data: req.session, setting: settings});
+        });
+    }
 });
 
 app.get('/pay-cotiz/', (req, res) => {
