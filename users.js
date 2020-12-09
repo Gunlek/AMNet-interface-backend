c2c2facbe7cba26009f457eb0dc7fe522d1528d4
Fabien Aubret <fabien.aubret@gmail.com>
2020-12-09 14:08:26 +0100

    (Settings) Add active prom's setting

--- users.js
+++ users.js
@@ -218,9 +218,15 @@ module.exports = (app) => {
         }
         else {
             connection.query('SELECT * FROM users WHERE user_id = ?', [req.session['user_id']], (_, results, __) => {
-                if(results.length > 0){
-                    res.render('users/profile.html.twig', {user_data: results[0]});
-                }
+                connection.query('SELECT * FROM settings', (error, settings_results, fields) => {
+                    let settings = {};
+                    settings_results.forEach(param => {
+                        settings[param['setting_name']] = param['setting_value'].replace(/<br\/>/g, '\n');
+                    });
+                    if(results.length > 0){
+                        res.render('users/profile.html.twig', {user_data: results[0], setting: settings});
+                    }
+                });
             });
         }
     });
