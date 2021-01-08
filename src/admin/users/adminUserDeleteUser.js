211625a5fff0a9b763f901d245cbc5464c9fee83
Fabien Aubret <fabien.aubret@gmail.com>
2021-01-08 18:21:13 +0100

    (Radius Delete) Add methods to delete user from radius

--- src/admin/users/adminUserDeleteUser.js
+++ src/admin/users/adminUserDeleteUser.js
@@ -1,14 +1,23 @@
 const { DatabaseSingleton } = require("../../utils/databaseSingleton");
+const { DeleteRadiusUser } = require("../../utils/radius/deleteRadiusUser");
 
 /*
  * Delete a user from users table based on its user_id (passed over GET)
 */
 const AdminUserDeleteUser = (req, res) => {
     let database = DatabaseSingleton.getInstance().getDatabase();
-    
-    database.query('DELETE FROM users WHERE user_id = ?', [req.params.user_id], () => {
-        res.redirect('/admin/users/');
-    });
+
+    const { user_id } = req.params;
+
+    database.query('SELECT * FROM users WHERE user_id = ?', [user_id], (err, results, fields) => {
+        if(results.length > 0){
+            DeleteRadiusUser(results[0]['username']);
+        }
+
+        database.query('DELETE FROM users WHERE user_id = ?', [user_id], () => {
+            res.redirect('/admin/users/');
+        });
+    })
 }
 
 module.exports = { AdminUserDeleteUser };
