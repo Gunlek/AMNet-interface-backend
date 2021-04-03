57283df32aa9bd7fefc6ce4220e0e6e0efbfaa82
Fabien Aubret <fabien.aubret@gmail.com>
2021-04-03 19:37:11 +0200

    :bug: (Signin) Fix proms being wrongly compared

--- statics/js/handle-gadz-only-form.js
+++ statics/js/handle-gadz-only-form.js
@@ -1,12 +1,12 @@
 let promsSelect = document.getElementById("user_proms_select");
 let promsLabel = document.getElementById("promsLabel");
-let firstYear = promsSelect.querySelectorAll("option")[0].value;
+let firstYear = parseInt(promsSelect.querySelectorAll("option")[2].value);
 
 checkGadzDisplay(promsSelect);
 
 function checkGadzDisplay(element){
-    let value = promsSelect.value;
-    let isGadz = value > firstYear;
+    let value = parseInt(element.value);
+    let isGadz = value < firstYear;
     !isGadz ? [...document.querySelectorAll(".localGadzOnly")].map(el => {el.style.display = "none";}) : [...document.querySelectorAll(".localGadzOnly")].map(el => {el.style.display = "block";});
     promsLabel.innerHTML = !isGadz ? "Promotion" : "Prom's";
 }
@@ -14,3 +14,7 @@ function checkGadzDisplay(element){
 promsSelect.addEventListener("change", (event) => {
     checkGadzDisplay(event.target);
 });
+
+document.getElementById("user_proms_text").addEventListener("input", (event) => {
+    checkGadzDisplay(event.target);
+})
