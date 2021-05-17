fb6ab4d0ba2ba9ca689d21974498bbf7afa17f0f
Fabien Aubret <fabien.aubret@gmail.com>
2021-05-17 19:38:01 +0200

    :sparkles: (Internet) Remove access from radius on user delete request

--- src/internet/internetDeleteRequest.js
+++ src/internet/internetDeleteRequest.js
@@ -1,4 +1,5 @@
 const { DatabaseSingleton } = require('../utils/databaseSingleton');
+const { DisableRadiusIOTConnection } = require('../utils/radius/disableRadiusIOTConnection');
 
 /*
  * Delete a request created by user
@@ -8,6 +9,9 @@ const InternetDeleteRequest = (req, res) => {
     
     database.query('SELECT * FROM access WHERE access_id = ? AND access_user = ?', [req.params.access_id, req.params.user_id], function(errors, results, fields){
         if(results.length > 0){
+            if(process.env.RADIUS === "true"){
+                DisableRadiusIOTConnection(results[0].access_mac);
+            }
             database.query('DELETE FROM access WHERE access_id = ?', [req.params.access_id]);
             res.redirect('/internet/');
         }
