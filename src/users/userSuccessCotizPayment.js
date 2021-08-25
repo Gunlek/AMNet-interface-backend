2a66eb6221184f2bcd858802ac30d3d7b123fd0d
Fabien Aubret <fabien.aubret@gmail.com>
2021-08-25 19:46:25 +0200

    :bug: (Radius) Fix radius not being updated by server-side request

--- src/users/userSuccessCotizPayment.js
+++ src/users/userSuccessCotizPayment.js
@@ -1,5 +1,4 @@
 const { DatabaseSingleton } = require('../utils/databaseSingleton');
-const { EnableRadiusConnection } = require('../utils/radius/enableRadiusConnection');
 
 require('dotenv').config();
 
@@ -8,14 +7,9 @@ const UserSuccessCotizPayment = (req, res) => {
     database.query('SELECT * FROM users WHERE user_id=?', [req.session.user_id], (errors, results, fields) => {
         if(results.length > 0){
             req.session['user_pay_status'] = results[0]['user_pay_status'];
-
-            if(req.session['user_pay_status'] == "1"){
-                if(process.env.RADIUS == "true"){
-                    EnableRadiusConnection(req.session['user_name']);
-                }
-            }
         }
     });
+    // TO FIX: Labelled as error but it's not one !!!
     res.render('errors/payment_redirection.html.twig');
 }
 
