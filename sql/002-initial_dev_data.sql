93b77103b6efe8b0a8e4df0d311d408462f80117
Fabien Aubret <fabien.aubret@gmail.com>
2021-08-26 23:55:06 +0200

    :wrench: (Docker) Add docker-compose for dev purposes and update sql data

---
+++ sql/002-initial_dev_data.sql
@@ -0,0 +1,28 @@
+INSERT INTO users (
+    user_name,
+    user_firstname,
+    user_lastname,
+    user_email,
+    user_phone,
+    user_password,
+    user_bucque,
+    user_fams,
+    user_proms,
+    user_campus,
+    user_pay_status,
+    user_rank
+)
+VALUES (
+    "dev",
+    "dev firstname",
+    "dev lastname",
+    "dev@dev.com",
+    "0000000000",
+    MD5("password"),
+    "dev bucque",
+    "58",
+    "218",
+    "li",
+    "1",
+    "admin"
+)
