8a8ae073da1bc87724239191da14dd990e8d9f06
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-06-02 22:15:08 +0200

    :sparkles: (Settings) Add Get setting and get admin-list

--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -1,31 +1,60 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Delete, Get, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
+import { ApiOperation, ApiProduces, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Database } from 'src/utils/database';
+import { Response } from 'express';
 
 @ApiTags('settings')
 @Controller('settings')
 export class SettingsController {
-  @Get()
-  all(): string {
-    return 'get all settings';
+  @ApiOperation({
+    summary: 'Get pseudos and numbers of registered admin in database',
+  })
+  @ApiResponse({ status: 200, description: 'List of admin' })
+  @ApiProduces('application/json')
+  @Get('adminlist')
+  async list(): Promise<any[]> {
+    const List = (await Database.promisedQuery(
+      'SELECT setting_value FROM settings WHERE setting_name="admin_pseudos" ||  setting_name="admin_nums"'
+    ));
+      
+    const pseudos = List[0].setting_value.split(';');
+    const nums = List[1].setting_value.split(';');
+    let adminList = [];
+
+    pseudos.map((pseudo: string, index: number) => {
+      adminList.push({ pseudo: pseudo, id: nums[index] })
+    });
+
+    return adminList;
   }
 
-  @Get(':id')
-  get(): string {
-    return 'get a specific setting by id';
-  }
+  @ApiOperation({
+    summary: 'Get specific setting by id',
+  })
+  @ApiResponse({ status: 200, description: 'A setting is returned' })
+  @ApiResponse({
+    status: 204,
+    description: 'No setting matching this id were found',
+  })
+  @ApiProduces('application/json')
+  @Get(':name')
+  async get(
+    @Res({ passthrough: true }) res: Response,
+    @Param('name') name: string,
+  ): Promise<any> {
+    const value = (await Database.promisedQuery(
+      'SELECT setting_value FROM settings WHERE setting_name=?',
+      [name],
+    )) as {setting_value: string}[];
 
-  @Delete(':id')
-  delete(): string {
-    return 'delete a specific setting';
-  }
-
-  @Post()
-  add(): string {
-    return 'create a setting';
+    if (value.length == 0) res.status(HttpStatus.NO_CONTENT);
+    return value[0].setting_value;
   }
 
   @Put(':id')
   update(): string {
     return 'update a setting by id';
   }
+
+  
 }
