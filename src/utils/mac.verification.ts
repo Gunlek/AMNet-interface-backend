a38054c604e28659e03a13a734108d4195f8efb1
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-06-06 19:19:11 +0200

    :sparkles: (Mac Verification) Add function to verify the format of mac

---
+++ src/utils/mac.verification.ts
@@ -0,0 +1,17 @@
+const replaceAll = (str: string, search: string, replace: string) => {
+    return str.split(search).join(replace);
+}
+
+const isMacAdress = (MacAddress: string) => {
+    const regex = /^([0-9A-F]{2}){5}([0-9A-F]{2})$/;
+
+    return regex.test(MacAddress)
+}
+
+export const MacAdressVerification = (MacAddress: string) => {
+    const newMacAddress = replaceAll(replaceAll(replaceAll(replaceAll(MacAddress, '-', ''), ':', ''), ' ', ''), ';', '').toUpperCase()
+    const Verification = isMacAdress(newMacAddress)
+
+    if (Verification) return newMacAddress
+    else return ""
+};
