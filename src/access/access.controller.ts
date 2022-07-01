d68c74976288bcfa74c005b5d8070162556d41c5
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-07-01 19:37:58 +0200

    :sparkles: (Access) Create route to update a mac address

--- src/access/access.controller.ts
+++ src/access/access.controller.ts
@@ -12,6 +12,13 @@ import { Transporter } from 'src/utils/mail';
 @ApiTags('access')
 @Controller('access')
 export class AccessController {
+  @Get('quantity')
+  async GetQuantity(
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<number> {
+    return (await Database.promisedQuery('SELECT `access_id` FROM `access` WHERE `access_state`="pending"') as { access_id: number }[]).length;
+  }
+
   @ApiOperation({
     summary: 'Get the full list of registered access in database',
   })
@@ -43,6 +50,41 @@ export class AccessController {
 
     if (access.length == 0) res.status(HttpStatus.NO_CONTENT);
     return access[0];
+  }
+
+  @ApiOperation({
+    summary: 'Update an Access from the specified acces id',
+  })
+  @ApiResponse({ status: 200, description: 'Mac address of this access has been updated' })
+  @ApiResponse({
+    status: 204,
+    description: 'No Access matching this id were found',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Mac address of this access has not been updated because it has the wrong format',
+  })
+  @ApiProduces('application/json')
+  @Put(':id')
+  async put(
+    @Res({ passthrough: true }) res: Response,
+    @Param('id') id: number,
+    @Body() new_access: { access_mac: string }
+  ): Promise<void> {
+    const old_access = (await Database.promisedQuery('SELECT access_mac FROM access WHERE access_id=?', [id])) as { access_mac: string }[];
+
+    if (old_access.length !== 0) {
+      const mac_adrress = MacAdressVerification(new_access.access_mac)
+      if (mac_adrress !== "") {
+        await Database.promisedQuery('UPDATE `access` SET `access_mac`=? WHERE 1 WHERE access_id=?', [mac_adrress, id])
+        res.status(HttpStatus.OK);
+      }
+      else res.status(HttpStatus.BAD_REQUEST);
+    }
+    else {
+      res.status(HttpStatus.NO_CONTENT);
+    }
+
   }
 
   @ApiOperation({
@@ -78,7 +120,7 @@ export class AccessController {
             'INSERT INTO `access`(`access_description`, `access_mac`, `access_proof`, `access_user`, `access_state`) VALUES (?, ?, ?, ?, ?)', [access.access_description, mac_adrress, access.access_proof, access.access_user, "pending"])
           await RadiusDatabase.promisedQuery('INSERT INTO `radusergroup`(`username`, `groupname`, `priority`) VALUES (?, ?, ?)', [mac_adrress, "Disabled-Users", 0])
           await RadiusDatabase.promisedQuery('INSERT INTO `radcheck`( `username`, `attribute`, `op`, `value`) VALUES VALUES (?, ?, ?, ?)', [mac_adrress, "Auth-Type", ":=", "Accept"])
-          
+
           return "Access is created";
         }
         else {
@@ -100,7 +142,7 @@ export class AccessController {
 
   @Delete(':id')
   async delete(@Res({ passthrough: true }) res: Response,
-  @Param('id') id: number): Promise<void> {
+    @Param('id') id: number): Promise<void> {
     await Database.promisedQuery('DELETE FROM `access` WHERE access_id=?', [id]);
     await RadiusDatabase.promisedQuery('DELETE FROM `radcheck` WHERE `username`=?', [id])
     await RadiusDatabase.promisedQuery('DELETE FROM `radusergroup` WHERE `username`=?', [id])
@@ -118,11 +160,11 @@ export class AccessController {
   ): Promise<void> {
     await Database.promisedQuery('UPDATE `access` SET `access_state`="active" WHERE access_id=?', [id]);
     await RadiusDatabase.promisedQuery('UPDATE `radusergroup` SET `groupname`="Enabled-Users" WHERE `username`=?', [id])
-    const access = await Database.promisedQuery('SELECT `access_description`, `access_user` FROM `access` WHERE 1 access_id=?', [id]) as {access_description: string, access_user: string}[];
+    const access = await Database.promisedQuery('SELECT `access_description`, `access_user` FROM `access` WHERE 1 access_id=?', [id]) as { access_description: string, access_user: string }[];
 
     const email = await Database.promisedQuery('SELECT `user_email` FROM `access` WHERE user_id=?', [access[0].access_user])[0].user_email as string;
     const text = "Votre demande d'accès pour l'ojebt " + access[0].access_description + " a été acceptée. <br> Vous pouvez dès maintenant le connecter à AMNet WI-Fi IoT"
-    const htmlstream = fs.createReadStream('./src/mail/templates/info.html').pipe(replace("<TEXT_HERE>", text))
+    const htmlstream = fs.createReadStream('./src/mail/templates/info.html').pipe(replace("<TEXT_HERE>", text)).pipe(replace('<tr><td style="text-align: center; font-size: 12px;  padding: 0 10px;" class="text">Pour vous désabonner <a style="text-decoration: none; color:#096A09;"href="https://amnet.fr/homepage/unsubscribe" target="_blank">Cliquez ici</a></td></tr>', ""))
 
     await Transporter.sendMail('Votre demande a été aceptée', htmlstream, [email]);
   }
@@ -135,4 +177,7 @@ export class AccessController {
     await Database.promisedQuery('UPDATE `access` SET `access_state`="declined" WHERE access_id=?', [id]);
     await RadiusDatabase.promisedQuery('UPDATE `radusergroup` SET `groupname`="Disabled-Users" WHERE `username`=?', [id])
   }
+
+
+  
 }
