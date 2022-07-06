4e01055a0132628c1d90b6553b2117077b1e60af
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-07-07 00:27:35 +0200

    :recycle: (Auth) Delete useless informations from token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,24 +10,16 @@ export class AuthService {
 
   async login(user: UserType) {
     const results = await Database.promisedQuery(
-      'SELECT user_id, user_password, user_rank, user_is_gadz, user_pay_status FROM users WHERE user_name=?',
+      'SELECT user_id, user_password FROM users WHERE user_name=?',
       [user.name],
-    ) as { user_id: string, user_password: string, user_rank: string, user_is_gadz: number, user_pay_status: number }[];
+    ) as { user_id: string, user_password: string }[];
 
     if (results.length === 1) {
       const vadlidPassword = await bcrypt.compare(user.password, results[0]['user_password'])
 
       if (vadlidPassword) {
-        const payload = {
-          username: user.name,
-          id: results[0]['user_id'],
-          is_gadz: results[0]['user_is_gadz'] == 1,
-          rank: results[0]['user_rank'],
-          pay_status: results[0]['user_pay_status'] == 1,
-        };
-
         return {
-          access_token: this.jwtService.sign(payload),
+          access_token: this.jwtService.sign({ id: results[0]['user_id'] })
         };
       }
       else {
