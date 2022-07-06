47ccb77c13729c731f5c8f5d1bf3ecc19f075ad9
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-07-07 00:26:47 +0200

    :sparkles: (Hardware) Add route to get all hardware requests

--- src/hardware/hardware.controller.ts
+++ src/hardware/hardware.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Delete, Get, Post, Put, Res } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Delete, Get, Param, Post, Put, Res } from '@nestjs/common';
+import { ApiOperation, ApiProduces, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Database } from 'src/utils/database';
 import { Response } from 'express';
+import { Access } from 'src/models/access.model';
 
 @ApiTags('hardware')
 @Controller('hardware')
@@ -11,6 +12,16 @@ export class HardwareController {
     @Res({ passthrough: true }) res: Response,
   ): Promise<number> {
     return (await Database.promisedQuery('SELECT `material_id` FROM `materials` WHERE `material_state`="pending"') as { material_id: number }[]).length;
+  }
+
+  @ApiOperation({
+    summary: 'Get the full list of registered access in database from an user',
+  })
+  @ApiResponse({ status: 200, description: 'List of access' })
+  @ApiProduces('application/json')
+  @Get('user/:id')
+  async userList(@Param('id') id: number,): Promise<any> {
+    return (await Database.promisedQuery('SELECT * FROM `materials` WHERE material_user=?', [id]));
   }
   
   @Post('add')
