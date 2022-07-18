f298be5a60f78e0a4ad70ed5787096d0f9b98ae0
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-07-18 03:30:00 +0200

    :recycle: (User Controller) Put the returned httpStatus in res.status

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Post,
   Put,
+  Res,
   UseGuards,
 } from '@nestjs/common';
 import {
@@ -23,32 +24,20 @@ import { Database } from 'src/utils/database';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CurrentUserOnly, Roles, RolesGuard } from 'src/auth/roles.guard';
 import { UserService } from './user.service';
+import { Response } from 'express';
 
 @ApiTags('user')
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
-  
-  @Get('quantity')
-  async GetQuantity(
-    @Res({ passthrough: true }) res: Response,
-  ): Promise<number> {
-    return (
-      (await Database.promisedQuery(
-        'SELECT `user_id` FROM `users` WHERE 1',
-      )) as { user_id: number }[]
-    ).length;
-  }
+  constructor(private userService: UserService) { }
 
-  @Get('quantity/paid')
-  async GetPaidQuantity(
-    @Res({ passthrough: true }) res: Response,
-  ): Promise<number> {
-    return (
-      (await Database.promisedQuery(
-        'SELECT `user_id` FROM `users` WHERE `user_pay_status`=1',
-      )) as { user_id: number }[]
-    ).length;
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(RolesGuard)
+  @Roles('admin')
+  @Get('quantity')
+  async getNumberOf(@Res({ passthrough: true }) res: Response): Promise<number[]> {
+    res.status(HttpStatus.OK);
+    return await this.userService.getNumberOfUsers();
   }
 
   @ApiOperation({
@@ -61,27 +50,24 @@ export class UserController {
   async update(
     @Body() user: User,
     @Param('id') id: number,
-  ): Promise<HttpStatus> {
-    return this.userService.updateUser(user, id);
+    @Res({ passthrough: true }) res: Response
+  ): Promise<void> {
+    res.status(await this.userService.updateUser(user, id));
   }
 
   @ApiOperation({
     summary: 'Create a user matching the provided informations',
   })
   @ApiResponse({ status: 200, description: 'A User is created' })
-  @ApiResponse({
-    status: 206,
-    description: 'No user is created because of a lack of information',
-  })
-  @ApiResponse({
-    status: 409,
-    description: 'No user is created because of email and/or name already used',
-  })
+  @ApiResponse({ status: 206, description: 'No user is created because of a lack of information' })
+  @ApiResponse({ status: 409, description: 'No user is created because of email and/or name already used' })
   @ApiConsumes('application/json')
   @ApiBody({ type: UserType })
   @Post()
-  async add(@Body() user: User): Promise<HttpStatus> {
-    return this.userService.createUser(user);
+  async add(@Body() user: User, @Res({ passthrough: true }) res: Response): Promise<any> {
+    const { httpStatus, error } = await this.userService.createUser(user);
+    res.status(httpStatus)
+    return error;
   }
 
   @ApiBearerAuth()
@@ -89,30 +75,36 @@ export class UserController {
     summary: 'Promote the user matching the provided user id to admin rank',
   })
   @ApiResponse({ status: 200, description: 'User promoted' })
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(RolesGuard)
   @Roles('admin')
   @Put('promote/:id')
-  async promote(@Param('id') id: number): Promise<HttpStatus> {
-    return this.userService.promoteUser(id);
+  async promote(@Param('id') id: number, @Res({ passthrough: true }) res: Response): Promise<void> {
+    res.status(await this.userService.promoteUser(id));
   }
 
   @ApiOperation({
     summary: 'Demote the user matching the provided user id to user rank',
   })
   @ApiResponse({ status: 200, description: 'User demoted' })
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(RolesGuard)
   @Roles('admin')
   @Put('demote/:id')
-  async demote(@Param('id') id: number): Promise<HttpStatus> {
-    return this.userService.demoteUser(id);
+  async demote(@Param('id') id: number, @Res({ passthrough: true }) res: Response): Promise<void> {
+    res.status(await this.userService.demoteUser(id));
   }
 
   @ApiOperation({
     summary: 'Delete the user matching the specified user id',
   })
   @ApiResponse({ status: 200, description: 'User deleted' })
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(RolesGuard)
   @Roles('admin')
   @Delete(':id')
-  async delete(@Param('id') id: number): Promise<HttpStatus> {
-    return this.userService.deleteUser(id);
+  async delete(@Param('id') id: number, @Res({ passthrough: true }) res: Response): Promise<void> {
+    res.status(await this.userService.deleteUser(id));
   }
 
   @Get('pay/:id')
@@ -122,10 +114,12 @@ export class UserController {
 
   @ApiResponse({ status: 200, description: 'User disabled' })
   @ApiResponse({ status: 404, description: 'No user exist with this id' })
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(RolesGuard)
   @Roles('admin')
   @Get('unpay/:id')
-  async unpay(@Param('id') id: number): Promise<HttpStatus> {
-    return this.userService.unpayUser(id);
+  async unpay(@Param('id') id: number, @Res({ passthrough: true }) res: Response): Promise<void> {
+    res.status(await this.userService.unpayUser(id));
   }
 
   @ApiOperation({
@@ -141,82 +135,58 @@ export class UserController {
     return (await Database.promisedQuery('SELECT * FROM users')) as User[];
   }
 
-  @ApiOperation({
-    summary: 'Get a single user from the specified user id',
-  })
+  @ApiOperation({ summary: 'Get a single user from the specified user id' })
   @ApiResponse({ status: 200, description: 'A User is returned' })
-  @ApiResponse({
-    status: 204,
-    description: 'No user matching this id were found',
-  })
+  @ApiResponse({ status: 204, description: 'No user matching this id were found' })
   @ApiProduces('application/json')
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(RolesGuard)
   @Roles('admin')
   @CurrentUserOnly('user')
   @Get(':id')
-  async get(@Param('id') id: number): Promise<User | HttpStatus> {
-    return (await this.userService.getUser(id))
-      ? this.userService.getUser(id)
-      : HttpStatus.NO_CONTENT;
+  async get(@Param('id') id: number, @Res({ passthrough: true }) res: Response): Promise<User> {
+    const user = await this.userService.getUser(id);
+
+    if (user) {
+      res.status(HttpStatus.OK);
+      return user;
+    }
+    else res.status(HttpStatus.NO_CONTENT);
   }
 
   @ApiOperation({
     summary: 'Return pseudo linked to the token',
   })
-  @ApiResponse({ status: 200, description: 'A User is created' })
+  @ApiResponse({ status: 200, description: 'The pseudo linked to the token' })
+  @ApiResponse({ status: 204, description: 'The token doesnt exist' })
   @ApiConsumes('application/json')
   @Get('password/:token')
-  async get(
+  async getPseudo(
     @Res({ passthrough: true }) res: Response,
     @Param('token') token: string,
   ): Promise<string> {
-    const user_id = (await Database.promisedQuery(
-      'SELECT token_user FROM reset_token WHERE token_value=?',
-      [token],
-    )) as { token_user: string }[];
+    const name = this.userService.getNameByToken(token)
 
-    if (user_id.length === 1) {
-      const user_name = (await Database.promisedQuery(
-        'SELECT user_name FROM users WHERE user_id=?',
-        [user_id[0].token_user],
-      )) as { user_name: string }[];
-
-      return user_name[0].user_name;
-    } else return 'No users found linked to this token';
+    if (name) {
+      res.status(HttpStatus.OK)
+      return name
+    }
+    else res.status(HttpStatus.NO_CONTENT)
   }
 
   @ApiOperation({
-    summary: 'Update user password by tokken',
+    summary: 'Update user password by token',
   })
   @ApiResponse({ status: 200, description: 'The password is upated' })
+  @ApiResponse({ status: 204, description: 'The token doesnt exist' })
+  @ApiResponse({ status: 409, description: 'The 2 passwords are not identical' })
   @ApiConsumes('application/json')
   @Put('password/:token')
   async reset_paswword(
     @Res({ passthrough: true }) res: Response,
     @Body() user: { password1: string; password2: string },
     @Param('token') token: string,
-  ): Promise<string> {
-    const user_id = (await Database.promisedQuery(
-      'SELECT token_user FROM reset_token WHERE token_value=?',
-      [token],
-    )) as { token_user: string }[];
-
-    if (user_id.length === 1) {
-      if (user.password1 === user.password2) {
-        const hashed_paswword = await bcrypt.hash(
-          user.password1,
-          Number(process.env.SALT_ROUND),
-        );
-        await Database.promisedQuery(
-          'UPDATE `users` SET `user_password`=? WHERE user_id=?',
-          [hashed_paswword, user_id[0].token_user],
-        );
-        await Database.promisedQuery(
-          'DELETE FROM `reset_token` WHERE token_value=?',
-          [token],
-        );
-
-        return 'Password is upated';
-      } else return 'The 2 passwords are not the same';
-    } else return 'No user found linked to this token';
+  ): Promise<void> {
+    res.status(await this.userService.updatePasswordByToken(token, user))
   }
 }
