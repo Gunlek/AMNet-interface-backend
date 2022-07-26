b432943714638b1789f8c494c215852f5e94e5d3
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-07-26 16:57:43 +0200

    :recycle: (Success Payment) Update payUser function

--- src/lydia/functions/successPayment.ts
+++ src/lydia/functions/successPayment.ts
@@ -4,17 +4,19 @@ import { HttpStatus } from "@nestjs/common";
 
 export const successPayment = async (ticket_id: string): Promise<HttpStatus> => {
     const user = await Database.promisedQuery(
-        'SELECT request_payer_id FROM lydia_transactions WHERE request_ticket = ?', 
+        'SELECT request_payer_id FROM lydia_transactions WHERE request_ticket = ?',
         [ticket_id]
     ) as { request_payer_id: number }[];
 
     if (user.length === 1) {
-        await Database.promisedQuery(
-            'DELETE FROM `lydia_transactions` WHERE request_ticket = ?', 
-            [ticket_id]
-        );
-        payUser(user[0].request_payer_id);
-
+        await Promise.all([
+            Database.promisedQuery(
+                'DELETE FROM `lydia_transactions` WHERE request_ticket = ?',
+                [ticket_id]
+            ),
+            payUser('several', [user[0].request_payer_id])
+        ]);
+        
         return HttpStatus.OK;
     }
 
