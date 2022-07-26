cce5bdcfd7ee19a45685dca1b17b7d52c9e78162
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-07-26 16:56:22 +0200

    :bug: (Lydia Service) Wrong function for successPayment

--- src/lydia/lydia.service.ts
+++ src/lydia/lydia.service.ts
@@ -1,6 +1,7 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { cancelPayment } from './functions/cancelPayment';
 import { startPayment } from './functions/startPayment';
+import { successPayment } from './functions/successPayment';
 
 @Injectable()
 export class LydiaService {
@@ -9,7 +10,7 @@ export class LydiaService {
     }
 
     successPayment(ticket_id: string): Promise<HttpStatus> {
-        return cancelPayment(ticket_id);
+        return successPayment(ticket_id);
     }
 
     startPayment(user_id: number): Promise<string> {
