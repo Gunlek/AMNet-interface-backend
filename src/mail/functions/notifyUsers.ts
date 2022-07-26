ad4d7b42dacc24f6ae2b2d9fedc260bcbb36bc84
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-07-26 16:58:51 +0200

    :bug: (Info Mail) Wrong way to manage recipients

--- src/mail/functions/notifyUsers.ts
+++ src/mail/functions/notifyUsers.ts
@@ -58,18 +58,19 @@ export const notifyUsers = async (body:
 
             const old_promotion = (Number(active_promotion) - 1).toString();
             const new_promotion = (Number(active_promotion) + Number(usins_state[0].setting_value) ? 1801 : 1).toString();
-            const user_pay_status = body.recipients.Contribution ? body.recipients.NoContribution ? "1, 0" : "1" : "0"
-            let user_proms = (body.recipients.OldPromotion ? old_promotion + "," : "") + (body.recipients.ActivePromotion ? active_promotion + "," : "") + (body.recipients.NewPromotion ? new_promotion : "")
+            const user_pay_status = body.recipients.Contribution ? body.recipients.NoContribution ? [1, 0] : 1 : 0;
+            let user_proms = [] as string[];
 
-            if (!body.recipients.NewPromotion) {
-                user_proms = user_proms.slice(0, -1)
-            }
+            if(body.recipients.OldPromotion) user_proms.push(old_promotion);
+            if(body.recipients.ActivePromotion) user_proms.push(active_promotion);
+            if(body.recipients.NewPromotion) user_proms.push(new_promotion);
 
             if (body.recipients.Other) {
-                const other = old_promotion + "," + active_promotion + "," + new_promotion
+                const other = [old_promotion, active_promotion, new_promotion];
 
                 email_users = (await Database.promisedQuery(
-                    'SELECT user_email FROM users WHERE user_pay_status IN (?) AND user_proms IN (?) OR user_proms NOT IN (?) AND `user_notification` = 1', [user_pay_status, user_proms, other]
+                    'SELECT user_email FROM users WHERE user_pay_status IN (?) AND user_proms IN (?) OR user_proms NOT IN (?) AND `user_notification` = 1', 
+                    [user_pay_status, user_proms, other]
                 )) as { user_email: string }[];
             }
             else {
@@ -84,7 +85,7 @@ export const notifyUsers = async (body:
     }
 
     if (email_users) {
-        let emails = [];
+        let emails = [] as string[];
 
         email_users.forEach((email) => { emails.push(email.user_email) })
         const htmlstream = fs.createReadStream('./src/mail/templates/info.html')
