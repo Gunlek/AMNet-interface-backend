946fe8fcc15dd61f9f5d630f8aa70a8f4e94095a
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-07-26 17:02:58 +0200

    :sparkles: (User) Can pass user[] in body to create multiple request

--- src/user/functions/unpayUser.ts
+++ src/user/functions/unpayUser.ts
@@ -2,44 +2,78 @@ import { HttpStatus } from '@nestjs/common';
 import { Database, RadiusDatabase } from 'src/utils/database';
 import { Gadzflix } from 'src/utils/jellyfin';
 
-export const unpayUser = async (id: number): Promise<HttpStatus> => {
-  const [user, mac_address] = await Promise.all([
-    Database.promisedQuery(
-      'SELECT user_name, gadzflix_id FROM users WHERE user_id=?',
-      [id],
-    ),
-    Database.promisedQuery(
-      'SELECT access_mac FROM access WHERE access_user=?',
-      [id],
-    )
-  ]) as [{ user_name: string, gadzflix_id: string }[], { acess_mac: string }[]]
-
-  if (user.length === 1) {
+export const unpayUser = async (type: "all" | "several", users?: number[]): Promise<HttpStatus> => {
+  if (type == "all") {
     let promise = [];
+
+    const [user, mac_address] = await Promise.all([
+      Database.promisedQuery(
+        'SELECT user_name, gadzflix_id FROM users'),
+      Database.promisedQuery(
+        'SELECT access_mac FROM access')
+    ]) as [{ gadzflix_id: string }[], { access_mac: string }[]]
 
     mac_address.forEach(async (access) => {
       promise.push(RadiusDatabase.promisedQuery(
         'UPDATE `radusergroup` SET `groupname`="Disabled-Users" WHERE `username`=?',
-        [access.acess_mac],
-      ))
+        [access.access_mac]
+      ));
+    });
+
+    user.forEach(async (user) => {
+      await Gadzflix.setIsDisabled(user.gadzflix_id, true)
     });
 
     promise.push(
       Database.promisedQuery(
-        'UPDATE `users` SET `user_pay_status`= 0 WHERE  user_id=?',
-        [id]
+        'UPDATE `users` SET `user_pay_status`= 0'
       ),
       RadiusDatabase.promisedQuery(
-        'UPDATE `radusergroup` SET `groupname`="Disabled-Users" WHERE `username`=?',
-        [user[0].user_name]
-      ),
-      Gadzflix.setIsDisabled(user[0].gadzflix_id, true)
+        'UPDATE `radusergroup` SET `groupname`="Disabled-Users"'
+      )
     );
 
     await Promise.all(promise);
+    return HttpStatus.OK;
+  }
+  else if (type == "several") {
+    let promise = [];
+    let user_names = [] as string[];
 
+    const [dbUsers, mac_address] = await Promise.all([
+      Database.promisedQuery(
+        'SELECT user_name, gadzflix_id FROM users WHERE user_id IN (?)', [users]),
+      Database.promisedQuery(
+        'SELECT access_mac FROM access WHERE access_user  IN (?)', [users])
+    ]) as [{ user_name: string, gadzflix_id: string }[], { acess_mac: string }[]]
+
+    mac_address.forEach(async (access) => {
+      promise.push(RadiusDatabase.promisedQuery(
+        'UPDATE `radusergroup` SET `groupname`="Disabled-Users" WHERE `username`=?',
+        [access.acess_mac]
+      ));
+    });
+
+    dbUsers.forEach(async (user) => {
+      user_names.push(user.user_name);
+    });
+
+    dbUsers.forEach(async (user) => {
+      await Gadzflix.setIsDisabled(user.gadzflix_id, true);
+    });
+
+    promise.push(
+      Database.promisedQuery(
+        'UPDATE `users` SET `user_pay_status`= 0 WHERE user_id IN (?)', [users]
+      ),
+      RadiusDatabase.promisedQuery(
+        'UPDATE `radusergroup` SET `groupname`="Disabled-Users" WHERE username IN (?)', [user_names]
+      )
+    );
+
+    await Promise.all(promise);
     return HttpStatus.OK;
   }
 
-  return HttpStatus.NOT_FOUND;
+  return HttpStatus.BAD_REQUEST;
 };
--- src/user/functions/updateStatus.ts
+++ src/user/functions/updateStatus.ts
@@ -2,21 +2,30 @@ import { HttpStatus } from '@nestjs/common';
 import { Database } from 'src/utils/database';
 import { Gadzflix } from 'src/utils/jellyfin';
 
-export const updateStatus = async (id: number): Promise<HttpStatus> => {
-    const user = await Database.promisedQuery(
-        'SELECT user_name, user_is_gadz, gadzflix_id FROM users WHERE user_id=?',
-        [id],
-    ) as { user_name: string, user_is_gadz: number, gadzflix_id: string }[];
+export const updateStatus = async (type: "all" | "several", users?: number[]): Promise<HttpStatus> => {
+    if (type == "all") {
+        const user = await Database.promisedQuery(
+            'SELECT user_is_gadz, `user_pay_status`, gadzflix_id FROM users'
+        ) as { user_is_gadz: boolean, gadzflix_id: string, user_pay_status: boolean }[]
 
-    if (user.length === 1) {
-        await Promise.all([
-            Database.promisedQuery(
-                'UPDATE `users` SET `user_is_gadz`=? WHERE user_id=?',
-                [!user[0].user_is_gadz, id]
-            ),
-            Gadzflix.setIsDisabled(user[0].gadzflix_id, !user[0].user_is_gadz)
-        ]);
 
+        user.forEach(async (user) => {
+            await Gadzflix.setIsDisabled(user.gadzflix_id, !(user.user_pay_status && user.user_is_gadz));
+        });
+
+        await Database.promisedQuery('UPDATE `users` SET `user_is_gadz`=NOT `user_is_gadz`');
+        return HttpStatus.OK;
+    }
+    else if (type == "several") {
+        const dbUsers = await Database.promisedQuery(
+            'SELECT user_is_gadz, gadzflix_id, `user_pay_status` FROM users WHERE user_id IN (?)', [users]
+        ) as { user_is_gadz: boolean, gadzflix_id: string, user_pay_status: boolean }[];
+
+        dbUsers.forEach(async (user) => {
+            await Gadzflix.setIsDisabled(user.gadzflix_id, !(user.user_pay_status && user.user_is_gadz));
+        });
+
+        await Database.promisedQuery('UPDATE `users` SET `user_is_gadz`=NOT `user_is_gadz` WHERE user_id IN (?)', [users]);
         return HttpStatus.OK;
     }
 
