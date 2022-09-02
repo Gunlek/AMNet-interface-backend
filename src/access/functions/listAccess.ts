79eb1050bd753f4dfdf58eaa853d389e3a481f65
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-09-02 05:12:05 +0200

    :sparkles: (Access) Delete from the list access for our devices

--- src/access/functions/listAccess.ts
+++ src/access/functions/listAccess.ts
@@ -2,5 +2,5 @@ import { Access } from "src/models/access.model"
 import { Database } from "src/utils/database"
 
 export const listAccess = async (): Promise<Access[]> => {
-    return await Database.promisedQuery('SELECT *, (SELECT `user_name` FROM `users` WHERE `user_id`=`access_user`) AS `user_name`, (SELECT `user_pay_status` FROM `users` WHERE `user_id`=`access_user`) AS `user_pay_status` FROM `access`  ') as Access[]
+    return await Database.promisedQuery('SELECT *, (SELECT `user_name` FROM `users` WHERE `user_id`=`access_user`) AS `user_name`, (SELECT `user_pay_status` FROM `users` WHERE `user_id`=`access_user`) AS `user_pay_status` FROM `access` WHERE `access_user` != "aeg"') as Access[]
 }
