8e9a4216188c8af5a922bc7b71bbc7681dddfab4
Fabien Aubret <fabien.aubret@gmail.com>
2025-09-09 23:07:18 +0200

    :bug: (Pay/Unpay) Fix possible empty array in SQL request

--- src/user/functions/payUser.ts
+++ src/user/functions/payUser.ts
@@ -44,41 +44,56 @@ export const payUser = async (type: "all" | "several", users?: number[]): Promis
     let user_names = [] as string[];
     let admin_names = [] as string[];
 
-    const [dbUsers, mac_address] = await Promise.all([
-      Database.promisedQuery(
-        'SELECT user_name, user_is_gadz,gadzflix_id, user_rank FROM users WHERE user_id IN (?)', [users]
-      ).catch(e => console.log('Failed to get user')),
-      Database.promisedQuery(
-        'SELECT access_mac, (SELECT `user_rank` FROM `users` WHERE `user_id`=`access_user`) AS `user_rank` FROM access WHERE access_user IN (?)', [users]
-      ).catch(e => console.log('Failed to get user\'s mac'))
-    ]) as [{ user_name: string, gadzflix_id: string, user_is_gadz: boolean, user_rank: string }[], { access_mac: string, user_rank: string }[]]
+    if(users && users.length > 0) {
+      const [dbUsers, mac_address] = await Promise.all([
+        Database.promisedQuery(
+          'SELECT user_name, user_is_gadz,gadzflix_id, user_rank FROM users WHERE user_id IN (?)', [users]
+        ).catch(e => console.log('Failed to get user')),
+        Database.promisedQuery(
+          'SELECT access_mac, (SELECT `user_rank` FROM `users` WHERE `user_id`=`access_user`) AS `user_rank` FROM access WHERE access_user IN (?)', [users]
+        ).catch(e => console.log('Failed to get user\'s mac'))
+      ]) as [{ user_name: string, gadzflix_id: string, user_is_gadz: boolean, user_rank: string }[], { access_mac: string, user_rank: string }[]]
 
-    mac_address.map(async (access) => {
-      promise.push(RadiusDatabase.promisedQuery(
-        'UPDATE `radusergroup` SET `groupname`=? WHERE `username`=?',
-        [access.user_rank == "admin" ? "Admins" : "Enabled-Users", access.access_mac]
-      ).catch(e => console.log('Failed to update groups')));
-    });
+      mac_address.map(async (access) => {
+        promise.push(RadiusDatabase.promisedQuery(
+          'UPDATE `radusergroup` SET `groupname`=? WHERE `username`=?',
+          [access.user_rank == "admin" ? "Admins" : "Enabled-Users", access.access_mac]
+        ).catch(e => console.log('Failed to update groups')));
+      });
 
-    dbUsers.map(async (user) => {
-      if (user.user_rank == "admin") admin_names.push(user.user_name);
-      else user_names.push(user.user_name);
-    });
+      dbUsers.map(async (user) => {
+        if (user.user_rank == "admin") admin_names.push(user.user_name);
+        else user_names.push(user.user_name);
+      });
 
-    promise.push(
-      Database.promisedQuery(
-        'UPDATE `users` SET `user_pay_status`=1 WHERE user_id IN (?)', [users]
-      ).catch(e => console.log('Failed to update user_pay_status=1')),
-      RadiusDatabase.promisedQuery(
-        'UPDATE `radusergroup` SET `groupname`="Enabled-Users" WHERE username IN (?)', [user_names]
-      ).catch(e => console.log('Failed to update Enabled-Users group')),
-      RadiusDatabase.promisedQuery(
-        'UPDATE `radusergroup` SET `groupname`="Admins" WHERE username IN (?)', [admin_names]
-      ).catch(e => console.log('Failed to update Admins group'))
-    );
+      promise.push(
+        Database.promisedQuery(
+          'UPDATE `users` SET `user_pay_status`=1 WHERE user_id IN (?)', [users]
+        ).catch(e => console.log('Failed to update user_pay_status=1'))
+      );
 
-    await Promise.all(promise);
-    return HttpStatus.OK;
+      if(user_names.length > 0) {
+        promise.push(
+          RadiusDatabase.promisedQuery(
+            'UPDATE `radusergroup` SET `groupname`="Enabled-Users" WHERE username IN (?)', [user_names]
+          ).catch(e => console.log('Failed to update Enabled-Users group'))
+        );
+      }
+
+      if(admin_names.length > 0) {
+        promise.push(
+          RadiusDatabase.promisedQuery(
+            'UPDATE `radusergroup` SET `groupname`="Admins" WHERE username IN (?)', [admin_names]
+          ).catch(e => console.log('Failed to update Admins group'))
+        );
+      }
+
+      await Promise.all(promise);
+      return HttpStatus.OK;
+    }
+    else {
+      return HttpStatus.BAD_REQUEST;
+    }
   }
 
   return HttpStatus.BAD_REQUEST;
--- src/user/functions/unpayUser.ts
+++ src/user/functions/unpayUser.ts
@@ -35,35 +35,42 @@ export const unpayUser = async (type: "all" | "several", users?: number[]): Prom
     let promise = [];
     let user_names = [] as string[];
 
-    const [dbUsers, mac_address] = await Promise.all([
-      Database.promisedQuery(
-        'SELECT user_name, gadzflix_id FROM users WHERE user_id IN (?)', [users]),
-      Database.promisedQuery(
-        'SELECT access_mac FROM access WHERE access_user  IN (?)', [users])
-    ]) as [{ user_name: string, gadzflix_id: string }[], { acess_mac: string }[]]
+    if(users && users.length > 0) {
+      const [dbUsers, mac_address] = await Promise.all([
+        Database.promisedQuery(
+          'SELECT user_name, gadzflix_id FROM users WHERE user_id IN (?)', [users]),
+        Database.promisedQuery(
+          'SELECT access_mac FROM access WHERE access_user  IN (?)', [users])
+      ]) as [{ user_name: string, gadzflix_id: string }[], { acess_mac: string }[]]
 
-    mac_address.map(async (access) => {
-      promise.push(RadiusDatabase.promisedQuery(
-        'UPDATE `radusergroup` SET `groupname`="Disabled-Users" WHERE `username`=?',
-        [access.acess_mac]
-      ));
-    });
+      mac_address.map(async (access) => {
+        promise.push(RadiusDatabase.promisedQuery(
+          'UPDATE `radusergroup` SET `groupname`="Disabled-Users" WHERE `username`=?',
+          [access.acess_mac]
+        ));
+      });
 
-    dbUsers.map(async (user) => {
-      user_names.push(user.user_name);
-    });
+      dbUsers.map(async (user) => {
+        user_names.push(user.user_name);
+      });
 
-    promise.push(
-      Database.promisedQuery(
-        'UPDATE `users` SET `user_pay_status`= 0 WHERE user_id IN (?)', [users]
-      ),
-      RadiusDatabase.promisedQuery(
-        'UPDATE `radusergroup` SET `groupname`="Disabled-Users" WHERE username IN (?)', [user_names]
-      )
-    );
+      promise.push(
+        Database.promisedQuery(
+          'UPDATE `users` SET `user_pay_status`= 0 WHERE user_id IN (?)', [users]
+        )
+      );
 
-    await Promise.all(promise);
-    return HttpStatus.OK;
+      if(user_names.length > 0) {
+        promise.push(RadiusDatabase.promisedQuery(
+            'UPDATE `radusergroup` SET `groupname`="Disabled-Users" WHERE username IN (?)', [user_names]
+          ).catch(e => console.log('Failed to update Enabled-Users group'))
+      ) ;
+      }
+
+      await Promise.all(promise);
+      return HttpStatus.OK;
+    }
+    else return HttpStatus.BAD_REQUEST;
   }
 
   return HttpStatus.BAD_REQUEST;
