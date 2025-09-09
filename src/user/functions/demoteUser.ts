57c5be1b446cf9b3107f7400bfaf07a1156329cd
Fabien Aubret <fabien.aubret@gmail.com>
2025-09-09 23:21:08 +0200

    :bug: (Typo) Fix missing quote in sql request

--- src/user/functions/demoteUser.ts
+++ src/user/functions/demoteUser.ts
@@ -3,7 +3,7 @@ import { Database, RadiusDatabase } from 'src/utils/database';
 
 export const demoteUser = async (id: number): Promise<HttpStatus> => {
   const name = await Database.promisedQuery(
-    'SELECT `user_name`, `user_pay_status FROM `users` WHERE user_id=?',
+    'SELECT `user_name`, `user_pay_status` FROM `users` WHERE user_id=?',
     [id],
   ) as { user_name: string, user_pay_status: boolean }[];
 
@@ -12,11 +12,11 @@ export const demoteUser = async (id: number): Promise<HttpStatus> => {
       Database.promisedQuery(
         'UPDATE users SET user_rank="user" WHERE user_id=?',
         [id],
-      ),
+      ).catch(() => console.log('[Demote User] Failed to update user')),
       RadiusDatabase.promisedQuery(
         'UPDATE `radusergroup` SET `groupname`=? WHERE `username`=?',
         [name[0].user_pay_status ? 'Enabled-Users' : 'Disabled-Users', name[0].user_name]
-      )
+      ).catch(() => console.log('[Demote User] Failed to update radusergroup'))
     ])
 
     return HttpStatus.OK;
